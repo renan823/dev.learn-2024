91de69bab14c54c42fc64f286502ecc32d13d1e0
renan823 <renan.trofino@gmail.com>
2024-05-22 20:49:31 -0300

    novas paginas

---
+++ src/Noticias.jsx
@@ -0,0 +1,9 @@
+function Noticias () {
+    return (
+        <div>
+            <h1>Noticias</h1>
+        </div>
+    )
+}
+
+export default Noticias
---
+++ src/Premios.jsx
@@ -0,0 +1,9 @@
+function Premios () {
+    return (
+        <div>
+            <h1>Premios</h1>
+        </div>
+    )
+}
+
+export default Premios
