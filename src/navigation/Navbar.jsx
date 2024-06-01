48df64b27ff7874bc1c9ecb28c9bf1478df5bfdb
renan823 <renan.trofino@gmail.com>
2024-06-01 16:39:18 -0300

    pagina home

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -5,13 +5,16 @@ function Navbar ({ style }) {
         <div className="flex self-center mb-4 z-10 items-center">
             <div className="flex w-1/2">
                 <a href="/">
-                    <img src={Logo} alt="logo" className="w-20 h-20"/>
+                    <div className="rounded-full p-1" style={{ backgroundColor: "#191946" }}>
+                        <img src={Logo} alt="logo" className="w-20 h-20"/>
+                    </div>
                 </a>
             </div>
             <div className="flex w-1/2 justify-end gap-8 px-2">
                <a href="/sobre" className="text-lg font-bold" style={style}>Sobre</a>
                <a href="/premios" className="text-lg font-bold" style={style}>Prêmios</a>
                <a href="/noticias" className="text-lg font-bold" style={style}>Notícias</a>
+               <a href="https://linktr.ee/gema.icmc.usp" target="_blank" className="text-lg font-bold" style={style}>Contatos</a>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import Baloes from "../assets/Baloes.png";
+import USP from "../assets/USP.png";
+import ICMC from "../assets/ICMC.png";
 
 function Home () {
 	return (
 		<Layout style={{ color: "white" }} layout={{ backgroundColor: "#191946" }}>
-			<h1>Home</h1>
+			<div className="flex">
+				<div className="flex-col w-1/2 items-center content-center">
+					<h1 className="text-6xl font-bold text-white w-4/5 my-2">O grupo de programação competitiva da USP</h1>
+					<h3 className="text-2xl text-white w-5/6 my-2">Quer aprender os algoritmos mais avançados e ganhar medalhas? Cola com a gente!</h3>
+					<a href="/sobre" className="my-2">
+					<div className="px-10 py-1 w-fit rounded-md" style={{ backgroundColor: "#C39F00" }}>
+						<h2 className="text-lg text-white">Conhecer</h2>
+					</div>
+					</a>
+				</div>
+				<div className="flex justify-center w-1/2">
+					<img src={Baloes} alt="baloes" className="w-4/5" />
+				</div>
+			</div>
 		</Layout>
 	)
 }
